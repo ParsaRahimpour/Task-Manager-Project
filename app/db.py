17988f551fea6604d14297f0316fa51f4618b393
ParsaRahimpour <parsarhmpr2019@gmail.com>
2026-06-22 12:07:14 +0330

    chore: add .env support to docker-compose

--- app/db.py
+++ app/db.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://team_user:team_password@localhost:5432/team_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
