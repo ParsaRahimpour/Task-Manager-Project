6efaa39b9528d6de58e3fa5df4d6845b7ec2c4da
Erfan <erfan.hosseiniFadravi@gmail.com>
2026-06-22 16:18:11 +0330

    feat: add simple authorization with error handling and a simple example db to work with

---
+++ app/main.py
@@ -0,0 +1,87 @@
+# In the name of Allah
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class loginRequest(BaseModel):
+    userID: int
+    password: str
+
+
+userAuth = -1
+
+tasks = [
+    {
+        'title': 'docker',
+        'userID': 1,
+        'taskID': 0
+    },
+    {
+        'title': 'git',
+        'userID': 0,
+        'taskID': 1
+    },
+    {
+        'title': 'docker-git',
+        'userID': 0,
+        'taskID': 2
+    },
+    {
+        'title': 'ubuntu',
+        'userID': 2,
+        'taskID': 3
+    }
+]
+
+users = [
+    {
+        'userID': 0,
+        'password': 'ali'
+    },
+    {
+        'userID': 1,
+        'password': 'gholi'
+    },
+    {
+        'userID': 2,
+        'password': 'maria'
+    },
+]
+
+
+@app.post('/login')
+def login(cred: loginRequest):
+    
+    global userAuth
+    
+    if userAuth != -1:
+        raise HTTPException(400, 'you are already loged in')
+    
+    for user in users:
+        if user['userID'] == cred.userID and user['password'] == cred.password:
+        
+            userAuth = user['userID']
+
+            return {
+                'message': 'successfully loged in'
+            }
+        
+    raise HTTPException(401, 'invalid credentials')
+
+
+
+@app.get('/logout')
+def logout():
+
+    global userAuth
+    
+    if userAuth == -1:
+        raise HTTPException(400, 'no login to logout')
+    
+    userAuth = -1
+
+    return {
+        'message': 'successful logout'
+    }
