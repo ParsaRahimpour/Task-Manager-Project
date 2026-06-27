21872044aadcf8e28598cbc81e940b5bbf7ae9c7
Sajjadkahouli <sajjadkaholi@gmail.com>
2026-06-27 12:00:29 +0330

    fix: add function's from database and delete classes

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 from fastapi import FastAPI , HTTPException
 import logging
+from database_api import get_completed_tasks_by_user as db_get_completed_tasks_by_user
 # Create FastAPI app instance
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-
-#classes
-class User:
-    id : int
-    name : str
-    email : str
-    
-class Task:
-    id : int
-    title : str
-    description : str
-    completed : bool
-    user_id : int
-
-users = {}
-tasks = {}
 
 
     
@@ -42,13 +27,16 @@ async def root():
 
 
 
-@app.get("/tasks/{completed}")
-async def get_completed_task(completed : str):
+@app.get("/tasks/completed/{id}")
+async def get_completed_task(id : int):
     try:
-        result = []
-        for task in tasks:
-            if task.completed == completed:
-                result.append(task)
+        if userAuth == -1:
+            raise HTTPException(401, 'not authorized')
+        result = get_all_tasks(id)
+        if result["code"] != 200:
+            raise HTTPException(status_code=result["code"], detail=result["message"])
+
+        
         if result == None:
             return {"error" : "Not found Any task that you want"}
         return {"success" , result}
